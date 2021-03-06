3bf9782d48b337270e763944e750bed4699acff2
Eitan Goldfrad <eitan.goldfrad@gmail.com>
2021-03-06 22:20:05 +0200

    init

---
+++ SDK2.xcframework/ios-arm64/SDK2.framework/Headers/SDK2.h
@@ -0,0 +1,19 @@
+//
+//  SDK2.h
+//  SDK2
+//
+//  Created by OCD on 19/07/2020.
+//  Copyright © 2020 Snappers. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SDK2.
+FOUNDATION_EXPORT double SDK2VersionNumber;
+
+//! Project version string for SDK2.
+FOUNDATION_EXPORT const unsigned char SDK2VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SDK2/PublicHeader.h>
+
+
